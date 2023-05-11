c150faaa215c0a3b26f562898cabe718ba2df10f
Ruan <ruanazsxdc123@gmail.com>
2023-05-11 16:58:34 -0300

    Editando toolbar e ajustes de css

--- poc/quill-text-editor/src/components/quill-editor.vue
+++ poc/quill-text-editor/src/components/quill-editor.vue
@@ -1,14 +1,12 @@
 <template>
-  <!-- Two-way Data-Binding -->
   <quill-editor
-    ref="myQuillEditor"
     v-model="content"
-    :options="editorOption"
-    :formats="['bold', 'italic']"
+    :options="editorOptions"
     @blur="onEditorBlur($event)"
     @focus="onEditorFocus($event)"
     @ready="onEditorReady($event)"
     @change="onEditorChange($event)"
+    :class="!this.teste ? 'hide-toolbar' : 'show-toolbar'"
   />
 </template>
 
@@ -16,16 +14,32 @@
 export default {
   data() {
     return {
-      content: "I am Example",
-      editorOption: {},
+      content:
+        "<p>dawopdjawiojdwajdioawj dioawjdio awjido jawiodjawio jdioaw jdijaw iodjawio djiawdj ioawdawopdjawiojdwajdioawj dioawjdio awjido jawiodjawio jdioaw jdijaw iodjawio djiawdj ioawvvvvvdawopdjawiojdwajdioawj dioawjdio awjido jawiodjawio jdioaw jdijaw iodjawio djiawdj ioawdawopdjawiojdwajdioawj dioawjdio awjido jawiodjawio jdioaw jdijaw iodjawio djiawdj ioawdawopdjawiojdwajdioawj dioawjdio awjido jawiodjawio jdioaw jdijaw iodjawio djiawdj ioaw</p>",
+      teste: false,
+      editorOptions: {
+        placeholder: "Digite um texto aqui.",
+        modules: {
+          toolbar: [
+            "bold",
+            "italic",
+            "strike",
+            "underline",
+            { list: "ordered" },
+            { list: "bullet" },
+          ],
+        },
+      },
     };
   },
   methods: {
     onEditorBlur(quill) {
       console.log("editor blur!", quill);
+      this.teste = false;
     },
     onEditorFocus(quill) {
       console.log("editor focus!", quill);
+      this.teste = true;
     },
     onEditorReady(quill) {
       console.log("editor ready!", quill);
@@ -35,10 +49,50 @@ export default {
       this.content = html;
     },
   },
-  computed: {
-    editor() {
-      return this.$refs.myQuillEditor.quill;
-    },
-  },
 };
 </script>
+
+<style lang="scss">
+.quill-editor {
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+  .ql-toolbar {
+    order: 2;
+  }
+  .ql-editor {
+    * {
+      overflow-y: scroll;
+      height: 95px;
+    }
+  }
+}
+.hide-toolbar .ql-toolbar {
+  display: none;
+}
+.show-toolbar {
+  .ql-toolbar {
+    display: block;
+  }
+  .ql-editor {
+    * {
+      overflow-y: hidden;
+      height: 100%;
+    }
+  }
+}
+.ql-active .ql-stroke {
+  stroke: green !important;
+}
+.ql-active .ql-fill {
+  fill: green !important;
+}
+button[class*="ql-"]:hover {
+  .ql-stroke {
+    stroke: green !important;
+  }
+  .ql-fill {
+    fill: green !important;
+  }
+}
+</style>
--- poc/quill-text-editor/src/main.js
+++ poc/quill-text-editor/src/main.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
-import 'quill/dist/quill.bubble.css' // for bubble theme
 import VueQuillEditor from 'vue-quill-editor'
 
 Vue.use(VueQuillEditor, /* { default global options } */)
